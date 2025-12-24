ad9820ecf6eb4dd70237088b948ad84b216f2e52
Collins Brohomah <morgankhweku@icloud.com>
2025-12-24 12:53:06 +0100

    Update settings for static files

--- punch_minichat/groupchat/views.py
+++ punch_minichat/groupchat/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.parsers import JSONParser
 from django.contrib.auth import get_user_model
 from .models import GroupChat, GroupMember, GroupMessage
 from .serializers import GroupChatSerializer, GroupMessageSerializer
