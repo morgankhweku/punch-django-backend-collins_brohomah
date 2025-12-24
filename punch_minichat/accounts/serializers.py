489d4d432f15e7765c49b114e38d1a631a25df00
Collins Brohomah <morgankhweku@icloud.com>
2025-12-24 11:52:02 +0100

    Update settings for static files

--- punch_minichat/accounts/serializers.py
+++ punch_minichat/accounts/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth.password_validation import validate_password
 User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, validators=[validate_password])
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
